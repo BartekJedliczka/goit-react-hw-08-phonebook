eb58e90e92e90ba59c407f33c3a05b981d4503f3
BartekJedliczka <bartekjedliczka@gmail.com>
2023-02-26 10:13:09 -0500

    Update ContactList.jsx

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,34 +1,72 @@
-import { selectFilteredContacts } from 'redux/selectors';
+import { selectContacts, selectFilter } from 'redux/contacts/selectors';
 import { useSelector, useDispatch } from 'react-redux';
-import { deleteContact } from 'redux/operations';
+import { removeContact } from 'redux/contacts/operations';
 import css from './ContactList.module.css';
 
 export const ContactList = () => {
   const dispatch = useDispatch();
-  const filteredContacts = useSelector(selectFilteredContacts);
+  const contacts = useSelector(selectContacts);
+  const filteredContacts = useSelector(selectFilter);
+  const contactsFilter = contacts.filter(contact =>
+    contact.name.toLowerCase().includes(filteredContacts)
+  );
+
   const handleDelete = id => {
-    dispatch(deleteContact(id));
+    dispatch(removeContact(id));
   };
 
   return (
-    <>
-      <ul className={css.list}>
-        {filteredContacts.map(({ id, name, number }) => (
-          <li className={css.item} key={id}>
-            <p>
-              {name}: {number}
-            </p>
-            <button
-              className={css.button}
-              type="button"
-              onClick={() => handleDelete(id)}
-              value="delete"
-            >
-              Delete contact
-            </button>
-          </li>
-        ))}
-      </ul>
-    </>
+    <ul className={css.list}>
+      {contactsFilter.map(({ id, name, number }) => (
+        <li className={css.item} key={id}>
+          <p className={css.label}>
+            {name}: {number}
+          </p>
+          <button
+            className={css.button}
+            type="button"
+            onClick={() => handleDelete(id)}
+            value="delete"
+          >
+            Delete contact
+          </button>
+        </li>
+      ))}
+    </ul>
   );
 };
+
+// import { selectFilteredContacts } from 'redux/contacts/selectors';
+// import { useSelector, useDispatch } from 'react-redux';
+// import { deleteContact } from 'redux/contacts/perations';
+// import css from './ContactList.module.css';
+
+// export const ContactList = () => {
+//   const dispatch = useDispatch();
+//   const filteredContacts = useSelector(selectFilteredContacts);
+//   const handleDelete = id => {
+//     dispatch(deleteContact(id));
+//   };
+
+//   return (
+//     <>
+//       <ul className={css.list}>
+//         {filteredContacts.map(({ id, name, number }) => (
+//           <li className={css.item} key={id}>
+//             <p>
+//               {name}: {number}
+//             </p>
+//             <button
+//               className={css.button}
+//               type="button"
+//               onClick={() => handleDelete(id)}
+//               value="delete"
+//             >
+//               Delete contact
+//             </button>
+//           </li>
+//         ))}
+//       </ul>
+//     </>
+//   );
+// };
